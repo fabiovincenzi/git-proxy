bc039b6d204d4e716b193b4500a551dcfcf8aef7
fabiovincenzi <fabio.vincenzi2001@gmail.com>
2025-04-03 15:35:40 +0200

    refactor(tsx): convert index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createBrowserHistory } from 'history';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 // core components
@@ -8,14 +7,12 @@ import Admin from './ui/layouts/Admin';
 import Login from './ui/views/Login/Login';
 import './ui/assets/css/material-dashboard-react.css';
 
-const hist = createBrowserHistory();
-
 ReactDOM.render(
-  <Router history={hist}>
+  <Router>
     <Routes>
-      <Route exact path='/admin/*' element={<Admin />} />
-      <Route exact path='/login' element={<Login />} />
-      <Route exact path='/' element={<Navigate from='/' to='/admin/repo' />} />
+      <Route path='/admin/*' element={<Admin />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/' element={<Navigate to='/admin/repo' />} />
     </Routes>
   </Router>,
   document.getElementById('root'),
